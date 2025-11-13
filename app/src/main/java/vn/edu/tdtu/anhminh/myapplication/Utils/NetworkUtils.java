5cb69649fa18c0a152691fb63259945dde307929
IvanTran0101 <ivanworkspace0101@gmail.com>
2025-11-13 22:26:20 +0700

    add NetworkUtils

--- app/src/main/java/vn/edu/tdtu/anhminh/myapplication/Utils/NetworkUtils.java
+++ app/src/main/java/vn/edu/tdtu/anhminh/myapplication/Utils/NetworkUtils.java
@@ -1,4 +1,43 @@
 package vn.edu.tdtu.anhminh.myapplication.Utils;
 
-public class NetworkUtils {
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public final class NetworkUtils {
+    private NetworkUtils(){
+
+    }
+
+    public static boolean isNetworkAvailable(Context context){
+        ConnectivityManager connMgr =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connMgr == null) {
+            return false;
+        }
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    public static boolean isWifiConnected(Context context){
+        ConnectivityManager connMgr =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connMgr == null){
+            return false;
+        }
+        NetworkInfo networkInfo =
+                connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    public static boolean isMobileConnected(Context context){
+        ConnectivityManager connMgr =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connMgr == null){
+            return false;
+        }
+        NetworkInfo networkInfo =
+                connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        return networkInfo != null && networkInfo.isConnected();
+    }
 }
