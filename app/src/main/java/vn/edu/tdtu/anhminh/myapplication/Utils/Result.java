20bc38e2ca363ef9d482c7e690c2c62c23cc6377
IvanTran0101 <ivanworkspace0101@gmail.com>
2025-11-13 22:40:58 +0700

    add Result

--- app/src/main/java/vn/edu/tdtu/anhminh/myapplication/Utils/Result.java
+++ app/src/main/java/vn/edu/tdtu/anhminh/myapplication/Utils/Result.java
@@ -1,4 +1,31 @@
 package vn.edu.tdtu.anhminh.myapplication.Utils;
 
-public class Result {
+public class Result<T> {
+    public enum Status{
+        SUCCESS,
+        ERROR,
+        LOADING
+    }
+
+    public final T data;
+    public final String message;
+    public final Status status;
+
+    private Result(Status status, T data, String message){
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+    public static <T> Result<T> success(T data){
+        return new Result<>(Status.SUCCESS,data, null);
+    }
+
+    public static <T> Result<T> error(T data){
+        return new Result<>(Status.ERROR,data, null);
+    }
+
+    public static <T> Result<T> loading(T data){
+        return new Result<>(Status.LOADING,data, null);
+    }
+
 }
